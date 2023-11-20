8b9e25bd6fc141c5082f204e2da9394cbdaa793a
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:16:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,347 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "The full stop is also known as___ in American English",
+
+         answer : "Period",
+
+         options : [
+
+                          "Longest pause",
+
+                          "Endpoint",
+
+                          "Period",
+
+                          "Close dot"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "Which of this sentence is should not have a full stop",
+
+         answer : "Come here! The teacher said.",
+
+         options : [
+
+                          "The door is open.",
+
+                          "Come here! The teacher said.",
+
+                          "Peter’s bag is big",
+
+                          "He asked if I should eat."  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "Where is the question mark used wrongly",
+
+         answer : "What a day?",
+
+         options : [
+
+                          "Where do you live ?",
+
+                          "You believe her ?",
+
+                          "What a day?",
+
+                          "CanItakeasip?"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "___ is used for special emphasis",
+
+         answer : "Exclamation mark",
+
+         options : [
+
+                          "Semicolon",
+
+                          "Exclamation mark",
+
+                          "Quotation mark",
+
+                          "Apostrophe"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "Some uses of the full stop include these except",
+
+         answer : "Marking of salutation",
+
+         options : [
+
+                          "Abbreviations",
+
+                          "Marking of salutation",
+
+                          "Indicating initials",
+
+                          "In numbering"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "Which of these is not a terminal punctuation",
+
+         answer : "Colon",
+
+         options : [
+
+                          "Colon",
+
+                          "Full stop",
+
+                          "Question mark",
+
+                          "Exclamation"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "Which of these is not a medial punctuation",
+
+         answer : "Exclamation",
+
+         options : [
+
+                          "Comma",
+
+                          "Semicolon",
+
+                          "Colon",
+
+                          "Exclamation"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "The most commonly used punctuation mark is",
+
+         answer : "Comma",
+
+         options : [
+
+                          "Comma",
+
+                          "Semicolon",
+
+                          "Colon",
+
+                          "Exclamation"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "What is the major role of the comma",
+
+         answer : "Separating sentence elements",
+
+         options : [
+
+                          "Introduction of items",
+
+                          "Separating sentence elements",
+
+                          "Joining words or parts of words together",
+
+                          "Addition of emphasis"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "Which medial punctuation expresses a heavier pause than the comma",
+
+         answer : "Semicolon",
+
+         options : [
+
+                          "Comma",
+
+                          "Semicolon",
+
+                          "Colon",
+
+                          "Exclamation"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "___ separates extra information from the rest of the sentence",
+
+         answer : "Parentheses",
+
+         options : [
+
+                          "Colon",
+
+                          "Semicolon",
+
+                          "Parentheses",
+
+                          "Quotation mark"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "___ performs a linking function",
+
+         answer : "Dash",
+
+         options : [
+
+                          "Semicolon",
+
+                          "Dash",
+
+                          "Apostrophe",
+
+                          "Comma"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "___ indicates contraction",
+
+         answer : "Apostrophe",
+
+         options : [
+
+                          "Semicolon",
+
+                          "Dash",
+
+                          "Apostrophe",
+
+                          "Comma"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:14,
+
+         question: "__ is used to show exact words of a speaker in the direct speech",
+
+         answer : "Quotation mark",
+
+         options : [
+
+                          "Colon",
+
+                          "Semicolon",
+
+                          "Parentheses",
+
+                          "Quotation mark"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
